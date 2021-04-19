05ed7d684ae84cfb036b331cd3d62f34830825cf
MosiichukO <Collegium12>
2021-04-19 21:45:02 +0300

    Update

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,6 +1,7 @@
 import com.google.gson.Gson;
 import entities.Category;
 import entities.Pet;
+import entities.User;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Assert;
@@ -15,80 +16,97 @@ public class ApiTests {
     String BASE_URL = "https://petstore.swagger.io/v2";
 
     @Test
-    public void addNewPetToTheStore() {
-        Category catCategory = new Category(131231231, "Cats");
+    public void addNewPet_myTest() {
+        Category snakeCategory = new Category(12, "Snakes");
 
-        System.out.println("I preparing test data...");
-        Pet petToAdd = Pet.builder()
-                .id(new Random().nextInt(3))
-                .category(catCategory)
-                .name("Jozzy")
+        System.out.println("Test data is preparing...");
+
+        Pet PetToBeAdded = Pet.builder()
+                .id(new Random().nextInt(4))
+                .category(snakeCategory)
+                .name("Kira")
                 .photoUrls(Collections.singletonList("urls"))
                 .tags(null)
                 .status("available")
                 .build();
-        System.out.println("Body to send: " + new Gson().toJson(petToAdd));
+
+        System.out.println("Body to send: " + new Gson().toJson(PetToBeAdded));
 
         Response addingPetResponse = given()
                 .baseUri(BASE_URL)
                 .basePath("/pet")
                 .contentType(ContentType.JSON)
-                .body(petToAdd)
+                .body(PetToBeAdded)
                 .when()
                 .post();
 
-        System.out.println("Response: " + addingPetResponse.asString());
-
         Pet addedPetResponse = addingPetResponse.as(Pet.class);
 
-        String expectedStatus = petToAdd.getStatus();
-        String actualStatus = addedPetResponse.getStatus();
+        System.out.println("Preparing for GET request by ID...");
 
-        Assert.assertEquals("Wrong status code", 200, addingPetResponse.getStatusCode());
-        Assert.assertEquals("Wrong status", expectedStatus, actualStatus);
+        long id = PetToBeAdded.getId();
+
+        Response gettingInfoAboutPet = given()
+                .baseUri(BASE_URL)
+                .pathParam("petId", id)
+                .when()
+                .get("/pet/{petId}");
+
+        System.out.println("Response GET: " + gettingInfoAboutPet.asString());
+
+        Pet gotPetResponse = gettingInfoAboutPet.as(Pet.class);
+
+        Assert.assertEquals("Wrong name", PetToBeAdded.getName(), gotPetResponse.getName());
     }
 
     @Test
-    public void changeInfoAboutPet() {
-        Category catCategory = new Category(131231231, "Cats");
+    public void registerNewUser_myTest () {
 
-        System.out.println("I preparing test data...");
-        Pet petToAdd = Pet.builder()
-                .id(new Random().nextInt(3))
-                .category(catCategory)
-                .name("Jozzy")
-                .photoUrls(Collections.singletonList("urls"))
-                .tags(null)
-                .status("available")
+        System.out.println("Test data is preparing...");
+
+        User userToBeRegistered = User.builder()
+                .id(5)
+                .username("Alex")
+                .firstName("Oleksandr")
+                .lastName("Mosiichuk")
+                .email("mosiychuk1998a@gmail.com")
+                .password("Alex121212")
+                .phone("0956278968")
+                .userStatus(2)
                 .build();
-        System.out.println("Body to send: " + new Gson().toJson(petToAdd));
 
-        Response addingPetResponse = given()
+        System.out.println("Body to send: " + new Gson().toJson(userToBeRegistered));
+
+        Response creatingNewUserResponse = given()
                 .baseUri(BASE_URL)
-                .basePath("/pet")
+                .basePath("/user")
                 .contentType(ContentType.JSON)
-                .body(petToAdd)
+                .body(userToBeRegistered)
                 .when()
                 .post();
 
-        System.out.println("Response: " + addingPetResponse.asString());
 
-        Pet addedPetResponse = addingPetResponse.as(Pet.class);
-        long petId = addedPetResponse.getId();
+        System.out.println("Response: " + creatingNewUserResponse.asString());
 
-        petToAdd.setName("Qwerty");
-        Pet changedPet = petToAdd;
+        User createdUserResponse = creatingNewUserResponse.as(User.class);
 
-        Response changePetIdResponse = given()
+
+        Assert.assertEquals("Status code in not 200", 200, creatingNewUserResponse.getStatusCode());
+
+        System.out.println("Preparing for GET request by ID...");
+
+        Response gettingInfoAboutUser = given()
                 .baseUri(BASE_URL)
-                .basePath("/pet")
-                .contentType(ContentType.JSON)
-                .body(changedPet)
+                .pathParam("username", "Alex")
                 .when()
-                .put();
+                .get("/user/{username}");
 
-        Pet changedPetFromResponse = changePetIdResponse.as(Pet.class);
+        System.out.println("Response GET: " + gettingInfoAboutUser.asString());
 
-        Assert.assertEquals("Wrong new name of pet", petToAdd.getName(), changedPetFromResponse.getName());
+        User gotUserResponse = gettingInfoAboutUser.as(User.class);
+
+        Assert.assertEquals("Wrong name", userToBeRegistered.getUsername(), gotUserResponse.getUsername());
+
     }
 }
+
